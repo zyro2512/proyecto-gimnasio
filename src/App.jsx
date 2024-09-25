f242d6ad9ae3a6a6c25709195c6a3052426baeb1
Martin Caresia <107585462+zyro2512@users.noreply.github.com>
2024-09-25 20:12:21 -0300

    Cambios en el archivo app.jsx y app.css

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,14 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <h1>Gimnasio</h1>
+          <h2>Desine Me Vexare</h2>
+      </header>
+      <nav><a href="">Membresias</a>
+      <a href="">Reservas de clases</a>
+      <a href="">rutinas de ejercicio</a>
+      </nav>
     </>
   )
 }
