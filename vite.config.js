e42eba2478290374f6d80221e39da9f793e0b256
Martin Caresia <107585462+zyro2512@users.noreply.github.com>
2024-09-27 21:50:42 -0300

    avance en la parte front

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig({ 
+  server: {
+    proxy: {
+      "/api": "http://localhost:5173",
+    }  
+  } ,
   plugins: [react()],
 })
