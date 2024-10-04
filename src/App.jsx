8ba6794edec597c19cfba1a1ce962dafb9a3987c
Martin Caresia <107585462+zyro2512@users.noreply.github.com>
2024-10-04 20:37:25 -0300

    codigo UseEffect archivo app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   const [activeSection, setActiveSection] = useState('membresias');
+  const [members, setMembers] = useState([]); // Estado para almacenar los datos de la API
 
   const handleNavClick = (section) => {
     setActiveSection(section);
   };
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/members', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then(response => response.json())
+    .then(data => setMembers(data)) // Almacenar los datos en el estado
+    .catch(error => console.error('Error:', error));
+  }, []); // El array vacío asegura que esto se ejecute solo una vez al montar el componente
 
   return (
     <>
@@ -18,11 +32,11 @@ function App() {
       </header>
       <nav>
          <a href="#membresias" onClick={() => handleNavClick('membresias')}>Membresías</a>
-         <a href="#reservas" onClick={() => handleNavClick('reservas')}>Reservas de clases</a>
+         <a href="#reservas" onClick={() => handleNavClick('reservas')}>Clases</a>
          <a href="#rutinas" onClick={() => handleNavClick('rutinas')}>Rutinas de ejercicio</a>
       </nav>
       <main>
-        {activeSection === 'membresias' && <Membresias />}
+        {activeSection === 'membresias' && <Membresias members={members} />}
         {activeSection === 'reservas' && <Reservas />}
         {activeSection === 'rutinas' && <Rutinas />}
       </main>
@@ -30,10 +44,65 @@ function App() {
   );
 }
 
-const Membresias = () => (
+const Membresias = ({ members }) => (
   <div>
     <h2>Membresías</h2>
     <p>Información sobre las membresías.</p>
+    <ul>
+      {members.map(member => (
+        <li key={member._id}>
+          {member.nombre} {member.apellido} - {member.membresia.tipo} ({new Date(member.membresia.fecha_inicio).toLocaleDateString()} - {new Date(member.membresia.fecha_fin).toLocaleDateString()})
+        </li>
+      ))}
+    </ul>
+    <form>
+      <label>
+        Nombre:
+        <br />
+        <input type="text" name="nombre" />
+      </label>
+      <br />
+      <label>
+        Apellido:
+        <br />
+        <input type="text" name="apellido" />
+      </label>
+      <br />
+      <label>
+        Fecha de Nacimiento:
+        <br />
+        <input type="date" name="fechaNacimiento" />
+      </label>
+      <br />
+      <label>
+        Altura:
+        <br />
+        <input type="number" name="altura" />
+      </label>
+      <br />
+      <label>
+        Peso:   
+       <br />
+        <input type="number" name="peso" />
+      </label>
+      <br />
+      <label>
+        Email:
+        <br />
+        <input type="email" name="email" />
+      </label>
+      <br />
+      <label>
+        Tipo de Membresía:
+        <br />
+        <select name="tipoMembresia">
+          <option value="mensual">Mensual</option>
+          <option value="anual">Anual</option>
+        </select>
+      </label>
+      <br />
+      <button type="submit">Enviar</button>
+    </form>
   </div>
 );
 
