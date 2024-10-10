94c2a001d30b9b71094f2739121be2cf0161a0e7
acvidela <acvidela@gmail.com>
2024-10-10 18:38:29 -0300

    pagina members con botón a agregar member

--- src/pages/memberList.jsx
+++ src/pages/memberList.jsx
@@ -2,7 +2,13 @@ import { useState, useEffect } from 'react';
 
 const MemberList = () => {
   const [members, setMembers] = useState([]);
+    // Estado para controlar qué componente mostrar (lista o detalle)
+    const [showDetails, setShowDetails] = useState(false);
 
+    // Función para alternar entre mostrar la lista de miembros o el detalle
+    const toggleDetails = () => {
+      setShowDetails(!showDetails);
+    };
   useEffect(() => {
     // Fetching members from the backend
     fetch('http://localhost:3000/api/members')
@@ -16,7 +22,7 @@ const MemberList = () => {
       <h3>Lista de Miembros</h3>
       <ul>
         {members.map(member => (
-          <li key={member._id}>{member.nombre} {member.apellido}</li>
+          <li key={member._id}>{member.nombre} {member.apellido}<button onClick={toggleDetails}>Más info</button></li>
         ))}
       </ul>
     </div>
